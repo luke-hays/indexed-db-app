51df80399c48f5282a8bb5fe096ec4fc22725539
Midival <lukehays32@gmail.com>
2023-01-16 22:40:06 -0500

    slight changes on test case setup

--- src/index.test.ts
+++ src/index.test.ts
@@ -13,16 +13,16 @@ describe('app should', () => {
   it('initially render with a message for no rows in DB')
 
   describe('have button Load DB onClick functionality include', () => {
-    it('onClick disables LoadDB, enables Query DB and Clear DB')
-    it('onClick fires loadDB event')
-    it('onClick causes notifications to render and be logged for load event')
+    it('disables LoadDB, enables Query DB and Clear DB')
+    it('fires loadDB event')
+    it('causes notifications to render and be logged for load event')
   })
 
   describe('have button Query DB onClick functionality include', () => {
-    it('onClick disables LoadDB, enables Query DB and Clear DB')
-    it('onClick fires queryDB event')
-    it('onClick causes notifications to render and be logged for query event')
-    it('should add text to query area after event')
+    it('disables LoadDB, enables Query DB and Clear DB')
+    it('fires queryDB event')
+    it('causes notifications to render and be logged for query event')
+		it('render text to query results with fields, including name, email, date last order, total sales')
   })
 
   describe('have button Clear DB onClick functionality include', () => {
@@ -31,5 +31,6 @@ describe('app should', () => {
     it('causes notifications to render and be logged for clear event')
     it('should remove text from query area after event')
   })
+
 })
 
